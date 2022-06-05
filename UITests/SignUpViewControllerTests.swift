1be899a57fb05dc90ea76e02c47d6eabf4095a1b
Fernando Tomio <nandotomio@gmail.com>
2022-06-05 17:21:01 -0300

    ensure SignUpViewController calls signUp with correct values

--- UITests/SignUpViewControllerTests.swift
+++ UITests/SignUpViewControllerTests.swift
@@ -4,25 +4,27 @@ import Presentation
 @testable import UI
 
 class SignUpViewControllerTests: XCTestCase {
-    func test_loading_is_hidden_on_start() throws {
+    func test_loading_is_hidden_on_start() {
         XCTAssertEqual(makeSut().loadingIndicator?.isAnimating, false)
     }
     
-    func test_sut_implements_loadingView() throws {
+    func test_sut_implements_loadingView() {
         XCTAssertNotNil(makeSut() as LoadingView)
     }
     
-    func test_sut_implements_alertView() throws {
+    func test_sut_implements_alertView() {
         XCTAssertNotNil(makeSut() as AlertView)
     }
     
-    func test_saveButton_calls_signUp_on_touch() throws {
-        var callsCount = 0
-        let sut = makeSut(signUpSpy: { _ in
-            callsCount += 1
-        })
+    func test_saveButton_calls_signUp_on_touch() {
+        var signUpViewModel: SignUpViewModel?
+        let sut = makeSut(signUpSpy: { signUpViewModel = $0 })
         sut.saveButton?.simulateTouch()
-        XCTAssertEqual(callsCount, 1)
+        let name = sut.nameTextField?.text
+        let email = sut.emailTextField?.text
+        let password = sut.passwordTextField?.text
+        let passwordConfirmation = sut.passwordConfirmationTextField?.text
+        XCTAssertEqual(signUpViewModel, SignUpViewModel(name: name, email: email, password: password, passwordConfirmation: passwordConfirmation))
     }
 }
 
