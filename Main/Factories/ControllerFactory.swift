a99627c877ea008f38047953e3f262f5ecaed4b4
Fernando Tomio <nandotomio@gmail.com>
2022-06-06 20:55:32 -0300

    change SignUp composition

---
+++ Main/Factories/ControllerFactory.swift
@@ -0,0 +1,17 @@
+import Foundation
+import UI
+import Presentation
+import Validation
+import Data
+import Infra
+import Domain
+
+class ControllerFactory {
+    static func makeSignUp(addAccount: AddAccount) -> SignUpViewController {
+        let controller = SignUpViewController.instantiate()
+        let emailValidatorAdapter = EmailValidatorAdapter()
+        let presenter = SignUpPresenter(alertView: controller, emailValidator: emailValidatorAdapter, addAccount: addAccount, loadingView: controller)
+        controller.signUp = presenter.signUp
+        return controller
+    }
+}
