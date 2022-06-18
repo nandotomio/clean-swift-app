b3f30298c2f96e9fe008ee9d3752d49fc3751a3a
Fernando Tomio <nandotomio@gmail.com>
2022-06-18 18:26:17 -0300

    improve code architecture

--- Main/SceneDelegate.swift
+++ Main/SceneDelegate.swift
@@ -6,26 +6,11 @@ import Data
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     
-    private let signUpFactory: () -> SignUpViewController = {
-        let alamofireAdapter = makeAlamofireAdapter()
-        let addAccount = makeRemoteAddAccount(httpClient: alamofireAdapter)
-        return makeSignUpController(addAccount: addAccount)
-    }
-    
-    private let loginFactory: () -> LoginViewController = {
-        let alamofireAdapter = makeAlamofireAdapter()
-        let authentication = makeRemoteAuthentication(httpClient: alamofireAdapter)
-        return makeLoginController(authentication: authentication)
-    }
-    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         let nav = NavigationController()
-        let welcomeRouter = WelcomeRouter(nav: nav, loginFactory: loginFactory, signUpFactory: signUpFactory)
-        let welcomeViewController = WelcomeViewController.instantiate()
-        welcomeViewController.signUp = welcomeRouter.gotoSignUp
-        welcomeViewController.login = welcomeRouter.gotoLogin
+        let welcomeViewController = makeWelcomeController(nav: nav)
         nav.setRootViewController(viewController: welcomeViewController)
         window?.rootViewController = nav
         window?.makeKeyAndVisible()
