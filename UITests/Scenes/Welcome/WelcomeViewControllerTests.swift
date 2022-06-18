86c603d009b5cb89b9f91819fbe58b10b8809388
Fernando Tomio <nandotomio@gmail.com>
2022-06-18 17:09:18 -0300

    ensure WelcomeViewController calls login when button is tapped

---
+++ UITests/Scenes/Welcome/WelcomeViewControllerTests.swift
@@ -0,0 +1,31 @@
+import XCTest
+import UIKit
+import Presentation
+@testable import UI
+
+class WelcomeViewControllerTests: XCTestCase {
+    func test_loginButton_calls_login_on_touch() {
+        let (sut, buttonSpy) = makeSut()
+        sut.loginButton?.simulateTouch()
+        XCTAssertEqual(buttonSpy.clicks, 1)
+    }
+}
+
+extension WelcomeViewControllerTests {
+    func makeSut() -> (sut: WelcomeViewController, buttonSpy: ButtonSpy) {
+        let buttonSpy = ButtonSpy()
+        let sut = WelcomeViewController.instantiate()
+        sut.login = buttonSpy.onClick
+        sut.loadViewIfNeeded()
+        checkMemoryLeak(for: sut)
+        return (sut, buttonSpy)
+    }
+    
+    class ButtonSpy {
+        var clicks = 0
+        
+        func onClick() {
+            clicks += 1
+        }
+    }
+}
